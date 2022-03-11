3bc1f1c31190816f889066543393725abbcbbef4
andrewyk99 <andrewyk99@hotmail.com>
2022-03-10 22:06:55 -0700

    read and use data from query parameter and handled errors

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -17,7 +17,7 @@ var getUserRepos = function(user) {
                 });
             }
             else {
-                alert("Error: GitHub User Not Found");
+                alert("Error: " + response.statusText);
             }
         })
         .catch(function(error) {
@@ -27,12 +27,16 @@ var getUserRepos = function(user) {
 };
 
 var formSubmitHandler = function(event) {
+    // prevents page from refreshing
     event.preventDefault();
     // get value from input element
     var username = nameInputEl.value.trim();
 
     if (username) {
         getUserRepos(username);
+
+        // clear old content
+        repoContainerEl.textContent = "";
         nameInputEl.value = "";
     }
     else {
@@ -48,8 +52,6 @@ var displayRepos = function(repos, searchTerm) {
         return;
     }
 
-    // clears old content
-    repoContainerEl.textContent = "";
     repoSearchTerm.textContent = searchTerm;
 
     // loop over repos
@@ -57,9 +59,10 @@ var displayRepos = function(repos, searchTerm) {
         // format repo name
         var repoName = repos[i].owner.login + "/" + repos[i].name;
 
-        // create a container for each repo
-        var repoEl = document.createElement("div");
+        // create a link for each repo
+        var repoEl = document.createElement("a");
         repoEl.classList = "list-item flex-row justify-space-between align-center";
+        repoEl.setAttribute("href", "./single-repo.html?repo=" + repoName);
 
         // create a span element to hold repository name
         var titleEl = document.createElement("span");
