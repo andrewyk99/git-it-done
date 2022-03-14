37518cd11e06ed5329d108761ed16056b3c58aca
Andrew kim <andrewyk99@hotmail.com>
2022-03-13 20:47:21 -0600

    function to call github search api's, display api data, language buttons, and call api function when buttons are clicked

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -2,6 +2,23 @@ var userFormEl = document.querySelector("#user-form");
 var nameInputEl = document.querySelector("#username");
 var repoContainerEl = document.querySelector("#repos-container");
 var repoSearchTerm = document.querySelector("#repo-search-term");
+var languageButtonsEl = document.querySelector("#language-buttons");
+
+// accepts language parameters, creates API endpoint, and makes HTTP request
+var getFeaturedRepos = function(language) {
+    var apiUrl = "https://api.github.com/search/repositories?q=" + language + "+is:featuredA&sort=help-wanted-issues";
+
+    fetch(apiUrl).then(function(response) {
+        if (response.ok) {
+            response.json().then(function(data) {
+                displayRepos(data.items, language);
+            });
+        }
+        else {
+            alert("Error: GitHub User Not Found");
+        }
+    });
+};
 
 var getUserRepos = function(user) {
     // format the github api url
@@ -41,6 +58,18 @@ var formSubmitHandler = function(event) {
     }
     else {
         alert("Please enter a GitHub username");
+    }
+};
+
+// button event handler for 'Search By Topics' buttons
+var buttonClickHandler = function(event) {
+    var language = event.target.getAttribute("data-language");
+
+    if (language) {
+        getFeaturedRepos(language);
+
+        // clear old content
+        repoContainerEl.textContent = "";
     }
 };
 
@@ -92,4 +121,6 @@ var displayRepos = function(repos, searchTerm) {
     }
 };
 
+// event listeners to form and button container
 userFormEl.addEventListener("submit", formSubmitHandler);
+languageButtonsEl.addEventListener("click", buttonClickHandler);
